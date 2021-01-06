d9cda1571bedadbb0ea6b0c22ff297f3c5e03677
Nakul Bharti <knakul853@gmail.com>
2021-01-06 23:43:00 +0530

    Add files via upload

---
+++ tour/src/Tours.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours, removeTours}) => {
+  return <section>
+    <div className="title">
+      <h2> ours tour </h2>
+      <div className="underline"></div>
+    </div>
+    <div>
+      {tours.map((tour) => {
+        return <Tour key={tour.id} {...tour} removeTours={removeTours}></Tour>
+      })}
+    </div>
+  </section>
+};
+
+export default Tours;
